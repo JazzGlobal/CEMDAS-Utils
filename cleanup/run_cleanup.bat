8c16d657e83cb33f18fd6949e0a059ef001a3686
JazzGlobal <waymer147@gmail.com>
2019-11-25 10:43:12 -0500

    Refactoring.

--- cleanup/run_cleanup.bat
+++ cleanup/run_cleanup.bat
@@ -2,4 +2,9 @@
 TITLE Generate Cleanup Scripts
 COLOR 7C
 py Generate_Cleanup_Script.py
+ECHO. 
+ECHO. 
+ECHO Scripts were generated in the %USERPROFILE%\DESKTOP\Generated_Scripts\ Directory. 
+ECHO Use caution when executing these scripts. 
+ECHO Double check all generated scripts for accuracy.
 PAUSE
