cb091fd667a89a86558928acb2dcb06a0baaf2e3
JazzGlobal <waymer147@gmail.com>
2019-11-25 10:20:28 -0500

    Refactoring: No longer relies on previously ran script to create the necessary directory

--- das_facts/Export_SysParams.py
+++ das_facts/Export_SysParams.py
@@ -5,6 +5,8 @@ import sys, os, openpyxl, pymssql
 from openpyxl.utils import get_column_letter
 
 database_name = input('Enter the name of the Sys1 database (Without Sys1)')
+
+# Database Connection Configuration
 connection = pymssql.connect(
                 server=f"{os.getlogin()}\\SQLEXPRESS",
                 port=1433,
@@ -12,9 +14,16 @@ connection = pymssql.connect(
                 password="sa123",
                 database=(f'{database_name}Sys1'))
 
-prefix_path = f'C:\\Users\\{os.getlogin()}\\Desktop\\Generated_Scripts\\'
-worksheet_path = f'{prefix_path}template_DasFacts.xlsx'
-work_book = openpyxl.load_workbook(worksheet_path)
+# File Location Configuration. 
+prefix_path = f'C:\\Users\\{os.getlogin()}\\Desktop\\Generated_Scripts\\' # Location of all generated files. 
+
+try: # Attempt to create directory. Continues script if directory already exists.
+    os.mkdir(prefix_path)
+except FileExistsError:
+    print(f'{prefix_path} already exists, continuing script.')
+
+workbook_path = f'{prefix_path}template_DasFacts.xlsx' # Path of workbook template. 
+work_book = openpyxl.load_workbook(workbook_path)
 sheet = work_book['Sheet1']
 cursor = connection.cursor()
 
@@ -24,6 +33,7 @@ def GetSystemParameters():
     for row in cursor.fetchall():  
         dataRow = { 
             # Adding the correct SQL Database field into our dictionary. 
+            # FORMAT:
             # Key in the dictionary: SQL Database Column data.
             "param_index": row[0],
             "param_name": row[1],
@@ -39,8 +49,10 @@ def GetSystemParameters():
         sqlData.append(dataRow)
     return sqlData
 
+# Create Sys_Param List
 toExport = GetSystemParameters()
 
+# Iterate through Sys_Params list.
 for parameter in range(len(toExport)):
     for db_field in range(len(toExport[parameter])):
         column_letter = get_column_letter(db_field+1)
