fdd6c39d5fa2e8517f0ece53f00b7261a61ed554
JazzGlobal <waymer147@gmail.com>
2019-11-25 15:05:55 -0500

    Now writes the SQL script to the log file.

--- cleanup/Generate_Cleanup_Script.py
+++ cleanup/Generate_Cleanup_Script.py
@@ -1,8 +1,6 @@
 # Generates SQL Scripts to perform the final clean up procedures for CEMDAS clients
 # !! Use caution when running the SQL scripts and be sure to double check the results !!
-import sys
-import os
-
+import sys, os
 prefix_path = f'C:\\Users\\{os.getlogin()}\\Desktop\\Generated_Scripts\\'
 
 try: # Attempt to create directory. Continues script if directory already exists.
@@ -14,6 +12,7 @@ sys_database_name = input('Enter the name of the Sys1 database, omit the \'Sys1\
 alarm_end_date = input("Enter the data of the first invalid alarm (YYYY-MM-DD): \n")
 calibration_end_date = input("Enter the data of the first invalid calibration (YYYY-MM-DD): \n")
 
+# Store into a string for usage in multiple locations.
 sql_string = ""
 sql_string+= f'USE [{sys_database_name}Sys2]\n'
 sql_string+= f'DELETE FROM ALARMS WHERE TIMESTAMP_OCCUR > \'{alarm_end_date}\';\n'
@@ -21,6 +20,10 @@ sql_string+= f'DELETE FROM CALIBRATION WHERE CAL_TIMESTAMP > \'{calibration_end_
 sql_string+= f'TRUNCATE TABLE SYS_LOG;\n'
 sql_string+= f'TRUNCATE TABLE ERROR_LOG;\n'
 
+# Write to SQL Script
 script = open(f'{prefix_path}cleanup.sql', 'w+')
 script.write(f'{sql_string}')
 
+# Write to Application Log file.
+application_log = open('application_log.log', 'a')
+application_log.write(sql_string)
