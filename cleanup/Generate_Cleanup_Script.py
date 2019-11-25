cb3e86878d7035e2f6f04ec1f9acf2bb7ac1f5ff
JazzGlobal <waymer147@gmail.com>
2019-11-25 14:00:33 -0500

    sql commands are now appened to a single string and then written to the file.

--- cleanup/Generate_Cleanup_Script.py
+++ cleanup/Generate_Cleanup_Script.py
@@ -9,14 +9,18 @@ try: # Attempt to create directory. Continues script if directory already exists
     os.mkdir(f'C:\\Users\\{os.getlogin()}\\Desktop\\Generated_Scripts')
 except FileExistsError:
     print(f'Directory {prefix_path} already exists, continuing script.')
-    
+
 sys_database_name = input('Enter the name of the Sys1 database, omit the \'Sys1\': \n')
 alarm_end_date = input("Enter the data of the first invalid alarm (YYYY-MM-DD): \n")
 calibration_end_date = input("Enter the data of the first invalid calibration (YYYY-MM-DD): \n")
 
+sql_string = ""
+sql_string+= f'USE [{sys_database_name}Sys2]\n'
+sql_string+= f'DELETE FROM ALARMS WHERE TIMESTAMP_OCCUR > \'{alarm_end_date}\';\n'
+sql_string+= f'DELETE FROM CALIBRATION WHERE CAL_TIMESTAMP > \'{calibration_end_date}\';\n'
+sql_string+= f'TRUNCATE TABLE SYS_LOG;\n'
+sql_string+= f'TRUNCATE TABLE ERROR_LOG;\n'
+
 script = open(f'{prefix_path}cleanup.sql', 'w+')
-script.write(f'USE [{sys_database_name}Sys2]\n')
-script.write(f'DELETE FROM ALARMS WHERE TIMESTAMP_OCCUR > \'{alarm_end_date}\';\n')
-script.write(f'DELETE FROM CALIBRATION WHERE CAL_TIMESTAMP > \'{calibration_end_date}\';\n')
-script.write(f'TRUNCATE TABLE SYS_LOG;\n')
-script.write(f'TRUNCATE TABLE ERROR_LOG;\n')
+script.write(f'{sql_string}')
+
