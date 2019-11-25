4c8e184673f61830a583153883f58ef6a00a91b7
JazzGlobal <waymer147@gmail.com>
2019-11-25 13:31:47 -0500

    Added spacing, color, and various visual changes.

--- das_facts/run_das_facts.bat
+++ das_facts/run_das_facts.bat
@@ -1,1 +1,10 @@
+@ECHO OFF
+TITLE Generate Das Facts Sheet
+COLOR 7C
+
 py Export_SysParams.py
+
+ECHO.
+ECHO.
+ECHO Das Facts Sheet was generated in the %USERPROFILE%\DESKTOP\Generated_Scripts\databasename Directory. 
+PAUSE
